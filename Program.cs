cec8b035797666d5fcc95079ead13b039144805e
Pedro Castaneda <petecast76@gmail.com>
2022-10-19 11:23:18 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Pedro Castaneda
+//10-19-2022
+//Restaurant Picker
+//Create a program that asks user what our of the four types of food they are in the mood for. When they pick a category it will randomly generate 1 restaurant out of 10. user can choose to regenerate again or return to main menu.
+//Peer Review is in READ.Me
+
+bool run = true;
+
+while(run)
+{
+    
+}
